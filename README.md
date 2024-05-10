1e34d8d8285f9efcb7c4ff18f08d4733bf459977
praabindhp <praabindhp@gmail.com>
2024-05-10 11:06:35 +0530

    Added hello-world Project

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ReactJS-Projects
 Projects From Basic To Advanced In ReactJS
+
+To Remove The GIT Config For Each Project
+```bash
+npx create-react-app project-name
+cd project-name
+rm -rf .git
+```
+
+In Windows
+```powershell
+rmdir /S .git
+```
---
+++ hello-world/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h3>Howdy, Developers</h3>
+        <p>
+          Its <code>Praabindh</code> Here
+        </p>
+        <a
+          className="App-link"
+          href="https://github.com/praabindhp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          GitHub Profile
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
